5263ef141664ee58139e5bb521de5c62ade50882
jdColonia <JuanDavidColoniaAldana@gmail.com>
2025-03-30 20:34:07 -0500

    fix: add route of vehicles to router

--- src/presentation/routes/index.ts
+++ src/presentation/routes/index.ts
@@ -1,5 +1,6 @@
 import authRoutes from "./auth.routes";
 import usersRoutes from "./users.routes";
+import vehiclesRoutes from "./vehicle.routes";
 
 const express = require('express');
 
@@ -7,5 +8,6 @@ const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/users', usersRoutes);
+router.use('/vehicles', vehiclesRoutes);
 
 module.exports = router;
